18fc4736a50b81472262ecaa9b28715b027190a0
AlexTicovschi <alexmticovschi@gmail.com>
2018-11-24 10:09:18 +0000

    updating books

--- server/server.js
+++ server/server.js
@@ -51,7 +51,15 @@ app.post('/api/book',(req,res)=>{
 })
 
 /*** UPDATE ***/
-
+app.post('/api/book_update',(req,res)=>{
+    Book.findByIdAndUpdate(req.body._id,req.body,{new:true},(err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.json({
+            success: true,
+            doc
+        })
+    })
+});
 
 /*** DELETE ***/
 
