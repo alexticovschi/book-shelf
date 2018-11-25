be188195a3fdbfe812cdb2c5244921fe1945e380
AlexTicovschi <alexmticovschi@gmail.com>
2018-11-25 13:21:26 +0000

    adding routes and structuring the app

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import books_reducer from './books_reducer';
+import user_reducer from './user_reducer';
 
 const rootReducer = combineReducers({
-
+    books_reducer,
+    user_reducer
 });
 
 export default rootReducer;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Home from './components/Home/home';
+
 const Routes = () => {
     return (
-        <div>
-            Hello, Schmuck!
-        </div>
+        <Switch>
+            <Route path="/" exact component={Home}/>
+        </Switch>
     );
 };
 
