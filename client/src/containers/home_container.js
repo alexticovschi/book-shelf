e3118658aecf9c3bfecba7c8b3e3f3965ed74a86
AlexTicovschi <alexmticovschi@gmail.com>
2018-11-26 16:25:32 +0000

    displaying books

--- client/src/containers/home_container.js
+++ client/src/containers/home_container.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { getBooks } from '../actions';
 
+import BookItem from '../widgetsUI/book_item';
+
 class HomeContainer extends Component {
     componentDidMount(){
         this.props.dispatch(getBooks(6,0,'desc'));
     }
 
+    renderItems = (books) => (
+        books.list ? 
+            books.list.map(item => (
+                <BookItem {...item} key={item._id}/>
+            ))
+        : null
+    )
+
     render() {
         console.log(this.props)
         return (
             <div>
-                Home Items
+                {this.renderItems(this.props.books)}
             </div>
         )
     }
