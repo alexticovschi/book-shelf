d5c46f7c0e8bb07e0508919ffbe810582c7a714e
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-01 11:03:28 +0000

    excluding and restricting  navlinks when user not logged in

--- client/src/components/Header/Sidenav/sidenav_items.js
+++ client/src/components/Header/Sidenav/sidenav_items.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import FontAwesome from 'react-fontawesome';
+import { connect } from 'react-redux'
 
-const SidenavItems = (props) => {
+const SidenavItems = ({user, onHideNav}) => {
 
     const items = [
         {
@@ -17,59 +18,66 @@ const SidenavItems = (props) => {
             icon: 'file-text-o',
             text: 'My Profile',
             link: '/user',
-            restricted: false
+            restricted: true
         },
         {
             type: 'navItem',
             icon: 'file-text-o',
             text: 'Add Admins',
             link: '/user/register',
-            restricted: false
+            restricted: true
         },
         {
             type: 'navItem',
             icon: 'file-text-o',
             text: 'Login',
             link: '/login',
-            restricted: false
+            restricted: false,
+            exclude: true
         },
         {
             type: 'navItem',
             icon: 'file-text-o',
             text: 'My Reviews',
             link: '/user/user-reviews',
-            restricted: false
+            restricted: true
         },
         {
             type: 'navItem',
             icon: 'file-text-o',
             text: 'Add Review',
             link: '/user/add',
-            restricted: false
+            restricted: true
         },
         {
             type: 'navItem',
             icon: 'file-text-o',
             text: 'Logout',
             link: '/user/logout',
-            restricted: false
+            restricted: true
         }
     ];
 
     const element = (item,i) => (
         <div key={i} className={item.type}>
-            <Link to={item.link} onClick={props.onHideNav}>
+            <Link to={item.link} onClick={onHideNav}>
                 <FontAwesome name={item.icon}/>
                 {item.text}
             </Link>
         </div>
     )
 
-    const showItems = () => {
-        return items.map((item,i) => {
-            return element(item,i);
+    const showItems = () => (
+        user.login ?
+            items.map((item,i) => {
+                if(user.login.isAuth) {
+                    return !item.exclude ? element(item,i) : null;
+                } else {
+                    return !item.restricted ? element(item,i) : null;
+                }
         })
-    }
+        : null
+    )
 
     return (
         <div>
@@ -78,4 +86,10 @@ const SidenavItems = (props) => {
     );
 };
 
-export default SidenavItems;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(SidenavItems);
